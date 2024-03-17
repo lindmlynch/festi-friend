8271eae28d75b96c0661c4cdccfb7251f93d5072
Linda Lynch <lindmlynch@gmail.com>
2024-03-17 15:23:47 +0000

    Firebase Auth implemented

--- app/src/main/java/ie/wit/festifriend/MainActivity.kt
+++ app/src/main/java/ie/wit/festifriend/MainActivity.kt
@@ -1,7 +1,10 @@
 package ie.wit.festifriend
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -10,11 +13,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import ie.wit.festifriend.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.lifecycle.Observer
+import ie.wit.festifriend.ui.auth.LoggedInViewModel
+import ie.wit.festifriend.ui.auth.Login
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var loggedInViewModel : LoggedInViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +43,28 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
+    public override fun onStart() {
+        super.onStart()
+        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
+
+        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+            if (loggedout) {
+                startActivity(Intent(this, Login::class.java))
+            }
+        })
+
+    }
+
     override fun onSupportNavigateUp(): Boolean {
 
         return findNavController(R.id.nav_host_fragment_activity_main).navigateUp()
                 || super.onSupportNavigateUp()
+    }
+
+    fun signOut(item: MenuItem) {
+        loggedInViewModel.logOut()
+        val intent = Intent(this, Login::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
 }
